427c52d813034bf47e5b07704fc95d03a20d17cd
alexdavis995 <60038185+alexdavis995@users.noreply.github.com>
2020-01-25 14:59:32 -0500

    Update math-program.sh

--- math-program.sh
+++ math-program.sh
@@ -1,5 +1,3 @@
-#! /bin/bash
-
 echo "Enter no 1: "
 read no1
 echo "Enter no 2:"
@@ -11,40 +9,21 @@ read no3
 echo
 echo "($no1 ^ $no2) + ($no3 * $no2)"
 
- m=$(( $no3 * $no2 ))
-
-
-
-counter=0
-ans=1
-while [ $no2 -ne $counter ]
-do
-        ans=`expr $ans \* $no1`
-        counter=`expr $counter + 1`
-done
-
-
+result="$(($((no1**$no2)) + $((no3*$no2))))"
 
 
 echo
 
 echo "RESULT: "
-echo $(( ans + m ))
+echo "$result"
 
 
 
-echo $(( ans + m ))>> build/myresult.txt
+echo $result>> build/myresult.txt
 
 echo
 
 echo "Type filename: "
 read filename
 
-echo $(( ans + m ))>> build/$filename.txt
-
-
-
-
-
-
-
+echo $result>> build/$filename.txt
